90198da7cd30b708178eeaf6097b409044248362
wingED-III <60010106@kmitl.ac.th>
2019-04-22 11:02:14 +0700

    Successfuly attach sqlite.

--- prac_sqlite/app/src/main/java/com/test0/pwing/prac_sqlite/MainActivity.java
+++ prac_sqlite/app/src/main/java/com/test0/pwing/prac_sqlite/MainActivity.java
@@ -1,10 +1,12 @@
 package com.test0.pwing.prac_sqlite;
 
 import android.database.Cursor;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javaSQL.SqliteHelper;
@@ -19,23 +21,37 @@ public class MainActivity extends AppCompatActivity {
         String[] namesLocation = {"place A", "placeB", "placeC"};
         String[] descript = {"aaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbb", "ccccccccccc"};
 
-        SqliteHelper myDB = new SqliteHelper(this, "mrt");
+        SqliteHelper myDB = new SqliteHelper(this);
 
         ListView listView = (ListView) findViewById(R.id.listView);
         ArrayList<MyBlock> arrayList = new ArrayList<>();
 
-        Cursor data = myDB.getList();
-//        while (data.moveToNext()) {
-//            arrayList.add(new MyBlock(data.getString(2), data.getString(3)));
-//        }
+
+        try {
+            myDB.createDB();
+        } catch (IOException e) {
+            throw new Error("Cannot Create Database");
+        }
+        try {
+            myDB.openDB();
+
+        } catch (SQLiteException e) {
+            throw e;
+        }
+
+        Cursor data = myDB.getTable();
+        //data.moveToFirst();
+        while (data.moveToNext()) {
+            arrayList.add(new MyBlock(data.getString(2), data.getString(3)));
+        }
 
 //        for (int i = 0; i < 3; i++) {
 //            arrayList.add(new MyBlock(namesLocation[i], descript[i]));
 //        }
 
-        for (int i = 0; i < 10; i++) {
-            arrayList.add(new MyBlock("insert title text here","insert text here"));
-        }
+//        for (int i = 0; i < 10; i++) {
+//            arrayList.add(new MyBlock("insert title text here", "insert text here"));
+//        }
 
 
         final Myadapter myadapter = new Myadapter(getBaseContext(), R.layout.mylistview_layout, arrayList);
--- prac_sqlite/app/src/main/java/javaSQL/SqliteHelper.java
+++ prac_sqlite/app/src/main/java/javaSQL/SqliteHelper.java
@@ -2,34 +2,106 @@ package javaSQL;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class SqliteHelper extends SQLiteOpenHelper {
-    private static final String DATABASE = "appDB";
+    private static final String DB_NAME = "appDB";
+    private static final String TABLE_NAME = "MRT_TABLE";
+    private static String DB_PATH;
     private static final int VERSION = 1;
-    private static String tableName;
+    private static Context CONTEXT;
+    private static SQLiteDatabase DATABASE;
+    private static String TOTAL_PATH;
 
-    public SqliteHelper(Context context,String tableName) {
-        super(context, DATABASE, null, VERSION);
-        this.tableName = tableName;
-//        String dbPath = "/data/data/"+ context.getPackageName()+"/"+"database/";
-//        Log.e("Path 1",dbPath);
+    public SqliteHelper(Context context) {
+        super(context, DB_NAME, null, VERSION);
+        this.CONTEXT = context;
+        //this.DB_PATH = this.CONTEXT.getDatabasePath(DB_NAME).toString();
+        this.DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        Log.d("DB_Path", this.DB_PATH);
+        this.TOTAL_PATH = this.DB_PATH+this.DB_NAME;
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String createCmd = "CREATE TABLE IF NOT EXISTS " + this.tableName + "(" +
-                "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
-                "id_station INTEGER, " +
-                "station TEXT, " +
-                "location TEXT)";
-        sqLiteDatabase.execSQL(createCmd);
+//        String createCmd = "CREATE TABLE IF NOT EXISTS " + this.TABLE_NAME + "(" +
+//                "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+//                "id_station INTEGER, " +
+//                "station TEXT, " +
+//                "location TEXT)";
+//        sqLiteDatabase.execSQL(createCmd);
+    }
+
+    private boolean checkDataBase() {
+        SQLiteDatabase checkDB = null;
+        try {
+            checkDB = SQLiteDatabase.openDatabase(this.TOTAL_PATH, null, SQLiteDatabase.OPEN_READONLY);
+        } catch (SQLiteException e) {
+            Log.d("myTag", e.getMessage());
+        }
+        if (checkDB != null) {
+            checkDB.close();
+            return true;
+        }
+        return false;
+    }
+
+    private void copyDB() throws IOException {
+        InputStream inputStream = this.CONTEXT.getAssets().open(this.DB_NAME);
+        String outFileName = this.TOTAL_PATH;
+        OutputStream outputStream = new FileOutputStream(outFileName);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, length);
+        }
+        outputStream.flush();
+        outputStream.close();
+        inputStream.close();
+    }
+
+    public void openDB() throws SQLException {
+        this.DATABASE = SQLiteDatabase.openDatabase(this.TOTAL_PATH, null, SQLiteDatabase.OPEN_READONLY);
+    }
+
+    public void createDB() throws IOException {
+        boolean hasNoDB = !this.checkDataBase();
+        if (hasNoDB) {
+            this.getReadableDatabase();
+            try {
+                this.copyDB();
+            } catch (IOException e) {
+                throw new Error("Error Copying Database");
+            }
+        }
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        onCreate(sqLiteDatabase);
+    public synchronized void close() {
+        if (this.DATABASE != null) {
+            this.DATABASE.close();
+        }
+        super.close();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        if (newVersion > oldVersion) {
+            try {
+                this.copyDB();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public Cursor queryWithSQL(String sql, String[] bindArgs) {
@@ -37,8 +109,23 @@ public class SqliteHelper extends SQLiteOpenHelper {
         return db.rawQuery(sql, bindArgs);
     }
 
-    public Cursor getList() {
-        SQLiteDatabase db = this.getReadableDatabase();
-        return db.rawQuery("SELECT * FROM " + this.tableName, null);
+    public Cursor getTable() {
+        Log.d("getTable", "executed");
+        String x = "__";
+        if (!checkDataBase()) {
+            x = x + "NOOOOO";
+        } else {
+            x = x + "OKKKKK";
+        }
+        Log.d("isDbNull", x);
+
+        return this.DATABASE.query(this.TABLE_NAME, null, null, null, null, null, null);
+
     }
+//
+//    public Cursor getList() {   //bug
+//        return this.DATABASE.rawQuery("SELECT * FROM " + this.TABLE_NAME, null);
+//    }
+
+
 }
